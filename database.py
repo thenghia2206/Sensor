7d289b467e1371f3178ca16537aec04c0c0c495c
Thế Nghĩa <105374517+thenghia2206@users.noreply.github.com>
2022-11-12 00:28:00 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
 
 
 SQLALCHEMY_DATABASE_URL = "postgres://qkiqebtcwazlxo:c3dc5b7b2f5bb613c21d053c5ab67aa11a74fd864409c86ff483825eeb9b24a7@ec2-54-174-31-7.compute-1.amazonaws.com:5432/d26mc3p1uaojah"
