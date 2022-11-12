a047b230000fbb6af6d120ac972bb234e06d5199
Thế Nghĩa <105374517+thenghia2206@users.noreply.github.com>
2022-11-13 01:19:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ async def sensor(temp : float ,hum: float ,db: Session = Depends(get_db) ):
     db.commit()
     db.refresh(x)
     return {'temperature': temp ,'humidity' : hum  }
+@app.delete("/delete")
+def delete_all(db: Session = Depends(get_db)):
+    db.query(models.Sensor).delete()
+    db.commit()
+    return {"OK"}
+
+@app.get("/get_new")
+def get_th(db: Session = Depends(get_db)):
+    data = db.query(models.Sensor).order_by(models.Sensor.id.desc()).first()
+    return {"temperature": data.temperature ,"humidity" : data.humidity}
 
 @app.get('/all')
 def get_all(db: Session = Depends(get_db)):
