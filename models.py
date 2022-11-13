17542989815c316eaa1294ff022e1a6dd25fb830
Thế Nghĩa <105374517+thenghia2206@users.noreply.github.com>
2022-11-14 00:00:59 +0700

    Update models.py

--- models.py
+++ models.py
@@ -2,16 +2,10 @@ from database import Base
 from sqlalchemy import Column,Integer,Float
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-from datetime import datetime
-import pytz
-
-tz_VN = pytz.timezone('Asia/Ho_Chi_Minh') 
-datetime_VN = datetime.now(tz_VN)
-
 
 class Sensor(Base):
     __tablename__= "sensor"
     id = Column(Integer, primary_key=True, nullable=False)
     temperature=Column(Float, nullable=False) 
     humidity=Column(Float, nullable=False)
-    createdOn=Column(TIMESTAMP(timezone=True),nullable=False, default=datetime_VN )
+    createdOn=Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
