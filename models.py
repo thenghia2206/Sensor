219b5226d9403dc8745bfaa5ded5b75b23ce7aec
Thế Nghĩa <105374517+thenghia2206@users.noreply.github.com>
2022-11-14 00:20:37 +0700

    Update models.py

--- models.py
+++ models.py
@@ -2,10 +2,13 @@ from database import Base
 from sqlalchemy import Column,Integer,Float
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from datetime import timedelta  
+
+x = datetime.datetime.now() + timedelta(hours=7) 
 
 class Sensor(Base):
     __tablename__= "sensor"
     id = Column(Integer, primary_key=True, nullable=False)
     temperature=Column(Float, nullable=False) 
     humidity=Column(Float, nullable=False)
-    createdOn=Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
+    createdOn=Column(TIMESTAMP(timezone=True),nullable=False, default=x )
